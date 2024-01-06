22e264989788271966bc2367cc7762c6e7568330
VanMinh153 <minhnguyenvh.703@gmail.com>
2024-01-06 14:03:26 +0700

    Jan 6 14:03

--- include/simulator.h
+++ include/simulator.h
@@ -5,10 +5,12 @@
 #define SSID_LEN 20
 #define PASSWORD_LEN 10
 #define DEFAULT_RESPONSE_TIME 100 // 100 ms
+// #define PASSWORD_DEFAULT "0000"
+
 
 // Environmental sensors measure humidity and concentrations of nutrients nitrogen (N), phosphorus (P), and potassium (K) in the soil. The sensors record data every T minutes.
 struct sensor_t {
-  unsigned int id;
+  int id;
   char ssid[SSID_LEN + 1];
   char password[PASSWORD_LEN + 1];
   unsigned int RESPONSE_TIME; // = DEFAULT_RESPONSE_TIME;
@@ -26,19 +28,20 @@ typedef struct sensor_t sensor_t;
 
 // The automatic watering unit irrigates simultaneously with 2 mechanisms: automatic watering at fixed times of the day set by the user if the soil moisture does not exceed the Hmax threshold and automatic watering if the soil moisture is below Hmin threshold. The Hmax and Hmin thresholds are set by the user.
 struct watering_machine_t {
-  unsigned int id;
+  int id;
   char ssid[SSID_LEN + 1];
   char password[PASSWORD_LEN + 1];
   char status;   // 1 if active, 0 if inactive
   unsigned int water_mount;  // liter
   time_t time_start;
   time_t time_end;
+  int sensor_id;
 };
 typedef struct watering_machine_t watering_machine_t;
 
 // Automatic NPK fertilization unit: when the concentration of nutrients is below the threshold of Nmin, Pmin, Kmin, this system automatically adds an amount of fertilizer mixed into the water at a concentration of C grams/liter and sprinkles an amount of V liters. Parameters set by the user.
 struct fertilizing_machine_t {
-  unsigned int id;
+  int id;
   char ssid[SSID_LEN + 1];
   char password[PASSWORD_LEN + 1];
   char status;      // 1 if active, 0 if inactive
@@ -46,15 +49,16 @@ struct fertilizing_machine_t {
   unsigned int P_mount; // gram/lit
   unsigned int K_mount; // gram/lit
   unsigned int water_mount;  // liter
+  int sensor_id;
 };
 typedef struct fertilizing_machine_t fertilizing_machine_t;
 
 // Photosynthesis support lamp part: glows with power P according to the time scheduled by the user.
 struct lamp {
-  unsigned int id;
+  int id;
   char ssid[SSID_LEN + 1];
   char password[PASSWORD_LEN + 1];
-  char status;  // 1 if active, 0 if inactive
+  char status;  // -1: NOt, 0 if inactive
   unsigned int power;    // watts
   time_t time_start;
   time_t time_end;
